b362fd27251abd89433ba9d17ebd41b3b4c1cf29
Kyle Welch <kwelch0626@gmail.com>
2017-05-13 23:13:59 -0500

    ⚡️ Init Commit

---
+++ package-scripts.js
@@ -0,0 +1,11 @@
+const { concurrent } = require('nps-utils');
+
+module.exports = {
+  scripts: {
+    test: 'jest',
+    flow: 'flow',
+    validate: concurrent.nps('test', 'flow'),
+    // no release so no need to babel and bundle
+    // build: 'babel index.js -d lib/',
+  },
+};
